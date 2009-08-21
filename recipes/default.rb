aed71a5ef342422a7adcc1ca2e9c292b968a66c9
bigfleet <jim@jimvanfleet.com>
2009-08-21 19:45:44 -0400

    OK, reference apache?

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,6 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+include_recipe "apache2"
 include_recipe "passenger_apache2::mod_rails"
 include_recipe "database"
 
